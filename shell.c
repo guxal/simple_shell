8895b9fa4edaf0a9d89fa8eddf6a2c05321d4fa3
JonnyCash <jonathanacp93@gmail.com>
2019-08-24 09:56:51 -0500

    add variables

--- shell.c
+++ shell.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * main - simple shell
  *
@@ -5,9 +9,27 @@
  */
 int main(void)
 {
+	char *buf = NULL;
+	char *token;
+	char *tmp;
+	char **argv;
+	int numTokens;
+	int characters = 0, i;
+	size_t bufsize = 0;
+
 	while (1)
 	{
-	
+		fflush(stdin);
+		characters = getline(&buf, &bufsize, stdin);
+
+		if (characters == -1)
+		{
+			perror("ERROR");
+		}
+		else
+		{
+
+		}
 	}
 
 	return (0);
