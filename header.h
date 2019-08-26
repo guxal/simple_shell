6e40f3baaa5001ae4089bd5e6305ce7db8854ef4
rolandoquiroz <rolandoquiroz@gmail.com>
2019-08-26 21:38:54 +0000

    header.h file added

--- header.h
+++ header.h
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void delete_memory(int **table, int value);
 int _strlen(char *str);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
