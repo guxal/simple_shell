ac7ed88c70e1ea48323cc1b934cf2eef942912a0
JonnyCash <jonathanacp93@gmail.com>
2019-08-25 23:30:26 -0500

    tmp calloc and add functions

--- shell.c
+++ shell.c
@@ -2,6 +2,86 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * *_calloc - call
+ * @nmemb: n
+ * @size: size
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *memory;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	memory = malloc(nmemb * size);
+	if (memory == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		*(memory + i) = 0;
+	return ((void *)memory);
+}
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+/**
+ * _strlen - string legth
+ * @str: string
+ * Return: integer length
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		++i;
+	return (i);
+}
+/**
+ * *_strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: copy destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int str = _strlen(src);
+	int i;
+
+	for (i = 0; i <= str; i++)
+	{
+			dest[i] = src[i];
+	}
+
+	return (dest);
+}
 /**
  * main - simple shell
  *
@@ -10,15 +90,18 @@
 int main(void)
 {
 	char *buf = NULL;
-	char *token;
+	//char *token;
 	char *tmp;
-	char **argv;
-	int numTokens;
-	int characters = 0, i;
+	//char **argv;
+	//int numTokens;
+	int characters = 0; 
+	//int i;
+	int status = 1;
 	size_t bufsize = 0;
 
-	while (1)
+	do
 	{
+		fflush(stdin);
 		characters = getline(&buf, &bufsize, stdin);
 		if (characters == -1)
 		{
@@ -26,9 +109,29 @@ int main(void)
 		}
 		else
 		{
-			printf("%s", buf);
+			printf("buf : %s", buf);
+			printf("strlen buf : %d\n", _strlen(buf));
+			//i = _strlen(buf);
+			//tmp = malloc(i * sizeof(char));	
+			//if (tmp == NULL)
+			//	exit(1);
+			tmp = _calloc((unsigned int) _strlen(buf) + 1, sizeof(char));
+			printf("sizeof : %lu\n", sizeof(tmp));
+			//printf("caracter %p\n", &tmp[1]);
+			tmp = _strcpy(tmp, buf);
+			printf("\n===== print bug ================\n");
+			simple_print_buffer(buf, _strlen(buf) + 1);
+			printf("\n===== print tmp ================\n");
+			simple_print_buffer(tmp, _strlen(tmp) + 1);
+			
+			//free(buf);
+			free(tmp);
+			//free(buf);
 		}
-	}
+		//free(tmp);
+	} while (status);
+
+	free(buf);
 
 	return (0);
 }
