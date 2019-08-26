04e74406bda1cd0aa20696c8ed7c3a5423bd08a0
JonnyCash <jonathanacp93@gmail.com>
2019-08-26 17:54:04 -0500

    delete comments

--- main.c
+++ main.c
@@ -8,35 +8,22 @@
 
 int main()
 {
-	/* me guarda el buf*/
 	char *buf = NULL;
-	/* me guarda el array resultado del strtok */
-	char *chainArray;
-	/* temporal */
-	char *temp;
-	/* me guarda un array de strings {"path", "path", "path"} */
 	char **argv;
-//int i;
 
 	while(EOF)
 	{
-		/* print prompt */
-		//if (isatty(fileno(stdin)))
+		if (isatty(fileno(stdin)))
 		printf("#cisfun$ ");
-
 		buf = read_line();
-
 		if (buf == NULL)
 		{
 			perror("NULL");
 		}
-		/* en caso de que sea exitosa */
 		else
 		{
 			argv = split_line(buf);
-
 			execute(argv);
-
 			free(buf);
 			free(argv);
 		}
